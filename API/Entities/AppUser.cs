45315ed1371cf2aa3b9610f359a96c51e35f865a
jamiekim828 <jamiekim828@gmail.com>
2023-06-19 14:09:52 +0200

    add columns in User database by migrations

--- API/Entities/AppUser.cs
+++ API/Entities/AppUser.cs
@@ -5,5 +5,14 @@ namespace API.Entities
         public int Id { get; set; }
 
         public string UserName { get; set; }
+
+        // after this, we need to add more column to database
+        // possible by migration
+        // API -> $ dotnet ef migrations add UserPasswordAdded
+        // and then $ dotnet ef database update
+
+        public byte[] PasswordHash { get; set; }
+
+        public byte[] PasswordSalt { get; set; }
     }
 }
